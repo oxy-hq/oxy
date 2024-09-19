74574a80eecfa09e67a7d0a5d7e946df132aebad
Robert Yi <rosiny@gmail.com>
2024-09-18 19:05:04 -0700

    only make config file if it doesn't exist
    GitOrigin-RevId: 197ef28bebcacccc7faad2c2223a22dddabef39d

--- src/init.rs
+++ src/init.rs
@@ -8,7 +8,7 @@ use std::error::Error;
 use std::result::Result;
 
 pub fn init() -> Result<(), Box<dyn Error>> {
-    // Step 1: Check for dbt-profiles.yml
+    // TODO: Step 1: Check for dbt-profiles.yml NONFUNCTIONAL
     if Path::new("dbt-profiles.yml").exists() {
         print!("dbt-profiles.yml found. Do you want to use this? (y/n): ");
         io::stdout().flush()?;
@@ -20,35 +20,41 @@ pub fn init() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    // Step 2: Create .onyx folder and config.yml
     let home_dir = home_dir().ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Home directory not found"))?;
     let onyx_dir = home_dir.join(".config").join("onyx");
-    fs::create_dir_all(&onyx_dir)?;
-
     let config_path = onyx_dir.join("config.yml");
-    let config_content = format!(r#"
+
+    if config_path.exists() {
+        println!("config.yml found in {}. Only initializing current directory.", config_path.display());
+        return Ok(());
+    } else {
+        // Step 2: Create .onyx folder and config.yml
+        fs::create_dir_all(&onyx_dir)?;
+
+        let config_content = format!(r#"
 warehouses:
-  - name: primary_warehouse
+- name: primary_warehouse
     type: bigquery
     key_path: /path/to/key
 
 models:
-  - name: openai-3.5
+- name: openai-3.5
     vendor: openai
     key_var: OPENAI_API_KEY
     model_ref: gpt-3.5-turbo
 
 defaults:
-  agent: default
-  project_path: {}
-"#, env::current_dir()?.display());
-    fs::write(config_path, config_content)?;
-    println!("Created .onyx/config.yml in home directory");
+agent: default
+project_path: {}
+    "#, env::current_dir()?.display());
+        fs::write(config_path, config_content)?;
+        println!("Created .config/onyx/config.yml in home directory");
+    }
 
     // Step 3: Use cargo-generate for project scaffolding
     println!("Creating project scaffolding...");
     let output = Command::new("cargo")
-        .args(&["generate", "--git", "https://github.com/dummy/repo", "--name", "onyx-project"])
+        .args(&["generate", "--git", "https://github.com/onyx-hq/onyx-sample-repo", "--name", "onyx-project"])
         .output()?;
 
     if output.status.success() {
