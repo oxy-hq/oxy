50698fba053caac4c421fdef4ad5cb4b2d743eb3
Robert Yi <rosiny@gmail.com>
2024-09-20 20:46:55 -0700

    Merge pull request #3 from onyx-hq/rsyi/use-agent-config
    Refactor: create an Agent class
    GitOrigin-RevId: 6fc7c3ab575413bc1b9d1dbad89fc8f03e066ddb

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-mod ai;
-mod chain;
+mod agent;
 mod init;
-mod python_interop;
 mod yaml_parsers;
 
 use clap::Parser;
-use reqwest::Client;
 use std::error::Error;
-use std::{env, fs, io};
 
-use crate::chain::process_input;
-use crate::yaml_parsers::config_parser::{get_config_path, parse_config, Config, ParsedConfig};
+use crate::agent::Agent;
+use crate::yaml_parsers::config_parser::{get_config_path, parse_config, Config};
+use crate::yaml_parsers::entity_parser::parse_entity_config_from_scope;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -46,7 +43,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             if !args.input.is_empty() {
                 let config_path = get_config_path();
                 let config = parse_config(config_path)?;
-                handle_input(&args.input, args.output.as_deref(), &config).await?;
+                handle_input(&args.input, &config).await?;
             } else {
                 println!(
                     "Use 'onyx init' to initialize a new project or provide a question/command."
@@ -58,33 +55,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn handle_input(
-    input: &str,
-    output_format: Option<&str>,
-    config: &Config,
-) -> Result<(), Box<dyn Error>> {
-    let ParsedConfig {
-        agent,
-        model,
-        warehouse,
-    } = config.load_defaults()?;
+async fn handle_input(input: &str, config: &Config) -> Result<(), Box<dyn Error>> {
+    let parsed_config = config.load_defaults()?;
+    let entity_config = parse_entity_config_from_scope(
+        &parsed_config.agent_config.scope,
+        &config.defaults.project_path,
+    )?;
 
-    let api_key = env::var(&model.key_var).map_err(|_| {
-        io::Error::new(
-            io::ErrorKind::NotFound,
-            format!("{} not set", model.key_var),
-        )
-    })?;
+    let agent = Agent::new(parsed_config, entity_config);
 
-    let client = Client::new();
-
-    process_input(
-        &agent,
-        &client,
-        &api_key,
-        input,
-        &model.model_ref,
-        &warehouse.key_path,
-    )
-    .await
+    agent.execute_chain(input).await
 }
