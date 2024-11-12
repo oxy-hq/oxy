9329320b017f8f3f5e83068795b941280dd69c14
Tay Nguyen <15603942+hotay@users.noreply.github.com>
2024-11-12 11:46:21 +0700

    Merge pull request #54 from onyx-hq/42-workflows-should-allow-for-specific-queries-to-be-executed-and-ingested
    GitOrigin-RevId: a2328a94d4b5d5168e77bad8d5b223f14bb274e4

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,12 @@
 use std::{fs, path::PathBuf};
 
+use syntect::{
+    easy::HighlightLines,
+    highlighting::{Style, ThemeSet},
+    parsing::SyntaxSet,
+    util::{as_24_bit_terminal_escaped, LinesWithEndings},
+};
+
 pub fn truncate_with_ellipsis(s: &str, max_width: usize) -> String {
     // We should truncate at grapheme-boundary and compute character-widths,
     // yet the dependencies on unicode-segmentation and unicode-width are
@@ -40,4 +47,19 @@ pub fn list_file_stems(path: &str) -> anyhow::Result<Vec<String>> {
         .map(|file| file.file_stem().unwrap().to_str().unwrap().to_string())
         .collect();
     Ok(names)
+}
+
+pub fn print_colored_sql(sql: &str) {
+    println!("\n\x1b[1;32mSQL query:\x1b[0m");
+    let ps = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
+    let syntax = ps.find_syntax_by_extension("sql").unwrap();
+    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+
+    for line in LinesWithEndings::from(sql) {
+        let ranges: Vec<(Style, &str)> = h.highlight_line(line, &ps).unwrap();
+        let escaped = as_24_bit_terminal_escaped(&ranges[..], false);
+        print!("{}", escaped);
+    }
+    println!();
 }
