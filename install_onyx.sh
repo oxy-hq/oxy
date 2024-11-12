d1e55aae49b9787ff7b5b22aa185a1b169dab6dd
luong-hq <luong@hyperquery.ai>
2024-11-12 12:27:18 +0700

    refactor: ensure that install dir work
    GitOrigin-RevId: c1b0063515a1dc5963f04ae32501b3836e01b113

--- install_onyx.sh
+++ install_onyx.sh
@@ -5,12 +5,34 @@ INSTALL_DIR="$HOME/.local/bin"
 CONFIG_DIR="$HOME/.config/onyx"
 CONFIG_FILE="config.yml"
 
+# Ensure the install directory exists
+mkdir -p "$INSTALL_DIR"
+
 # Get the latest release tag from GitHub API
 LATEST_TAG=$(curl -s https://api.github.com/repos/$REPO/releases/latest | grep 'tag_name' | cut -d\" -f4)
 
 # Determine the OS and architecture
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
+
+# Ensure the install directory is in the PATH
+if [[ ":$PATH:" != *":$INSTALL_DIR:"* ]]; then
+    echo "The install directory is not in your PATH. Adding it now..."
+    SHELL_NAME=$(basename "$SHELL")
+    case "$SHELL_NAME" in
+    bash)
+        echo "export PATH=\$PATH:$INSTALL_DIR" >> "$HOME/.bashrc"
+        source "$HOME/.bashrc"
+        ;;
+    zsh)
+        echo "export PATH=\$PATH:$INSTALL_DIR" >> "$HOME/.zshrc"
+        source "$HOME/.zshrc"
+        ;;
+    *)
+        echo "Unsupported shell: $SHELL_NAME. Please add $INSTALL_DIR to your PATH manually before installing this tool"
+        ;;
+    esac
+fi
 
 # Map architecture to target
 case $ARCH in
