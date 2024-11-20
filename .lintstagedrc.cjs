9f652ae9ee8a9db50d0954b861acfc3f9541f016
luong-hq <luong@hyperquery.ai>
2024-11-20 14:33:25 +0700

    ci: enable cargo check to run on main
    GitOrigin-RevId: 45678bf4a980ffb3d0e4c83ab396d32fb2c377bb

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   "**/*.{js,ts,tsx}": ["eslint --fix", "prettier --write"],
   "**/*.{ts,tsx}": ["bash -c 'pnpm turbo lint:ts'"],
-  "**/*.rs": ["cargo fmt --check"],
+  "**/*.rs": ["cargo fmt --check -- "],
   "**/*.{html,json,css,scss,md,mdx}": ["prettier -w"],
 };
