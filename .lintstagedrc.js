d2c8b7f1af3e55e6ee58cebc2db214d8ca6cbee9
luong-hq <luong@hyperquery.ai>
2025-02-07 00:34:29 +0700

    ci: update CI workflows and linting configurations
    Signed-off-by: luong-hq <luong@hyperquery.ai>
    GitOrigin-RevId: b0484219b82bad1719c5b26ee2e3d51bfc2dde0e

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,6 @@ export default {
   "**/*.{js,ts,tsx}": ["eslint --fix", "prettier --write"],
   "**/*.{html,css,scss,ts,tsx}": "pnpm dlx prettier --write",
   "**/*.{ts,tsx}": ["bash -c 'pnpm turbo lint:ts'"],
-  "**/*.rs": ["cargo fmt --check -- "],
+  "**/*.rs": ["cargo fmt --all --check -- "],
   "**/!(json-schemas)/*.{html,json,css,scss,md,mdx}": ["prettier -w"],
 };
