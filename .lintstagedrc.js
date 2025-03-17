bd18f86a3e322210a95edc5c5d80d65572e30cf1
luong-onyx <luong@onyxint.ai>
2025-03-17 14:46:14 -0700

    chore: remove remark for markdown
    GitOrigin-RevId: 631eac2287f3c451cad277d91df7cb674f5f3f4d

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,8 +3,4 @@ export default {
   "**/*.{ts,tsx}": ["bash -c 'pnpm turbo lint:ts'"],
   "**/*.rs": ["cargo fmt --all --check -- "],
   "**/!(json-schemas)/*.{html,json,css,scss,md,mdx}": ["prettier -w"],
-  "**/*.{mdx,md}": [
-    "eslint --fix",
-    "remark --output --ignore-path .remarkignore --",
-  ],
 };
