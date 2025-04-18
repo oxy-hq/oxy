7f0d2441013cc1ae48eb4e099010d7d4f5e4736d
Nhhhh <lenhattanh95@gmail.com>
2025-04-18 07:41:37 +0700

    fix: improve visualize tool output (#638)
    GitOrigin-RevId: 3eecb4574e68336fe3abe0850ac5625ed5cdeda2

--- web-app/src/components/AnswerContent/react-markdown.d.ts
+++ web-app/src/components/AnswerContent/react-markdown.d.ts
@@ -4,7 +4,7 @@ import React from "react";
 
 declare module "react-markdown" {
   export type ExtendedComponents = Components & {
-    chart?: React.ComponentType<{ file_path: string }>;
+    chart?: React.ComponentType<{ chart_src: string }>;
     reference?: React.ComponentType<{ children: React.ReactNode }>;
   };
 }
