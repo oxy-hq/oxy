56ba90b1bb96d18f9442d1abc0e81e91453bbf9b
Luong Vo <vo.tran.thanh.luong@gmail.com>
2025-04-22 19:23:33 -0700

    chore: set base for app
    GitOrigin-RevId: a9cdaeac6ac436b3295ae4dbefad00035bdb56e0

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -8,6 +8,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
