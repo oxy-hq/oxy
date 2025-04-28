74a7794b0022b8d24c979d8b43670accd625ced8
Luong Vo <vo.tran.thanh.luong@gmail.com>
2025-04-28 11:12:00 -0700

    chore: update CI workflow  (#682)
    GitOrigin-RevId: 71c845f8d35f5a65739976086b652d100a313ab5

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,7 @@
 export default {
   "**/*.{js,ts,tsx}": ["eslint --fix", "prettier --write"],
   "**/*.{ts,tsx}": ["bash -c 'pnpm turbo lint:ts'"],
-  "**/*.rs": ["cargo fmt --all --check -- "],
+  // eslint-disable-next-line no-undef
+  ...(process.env.CI ? {} : { "**/*.rs": ["cargo fmt --all --check -- "] }),
   "**/!(json-schemas)/*.{html,json,css,scss,md,mdx}": ["prettier -w"],
 };
