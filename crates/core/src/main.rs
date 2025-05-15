8682ff75e47f744c82982531f3816bf22f27e930
Luong Vo <vo.tran.thanh.luong@gmail.com>
2025-05-14 21:15:09 -0700

    refactor: unify stdout and file logging through appender (#778)
    GitOrigin-RevId: 3cffe5e3eabf24510efc7724a41d70050cb475df

--- crates/core/src/main.rs
+++ crates/core/src/main.rs
@@ -5,43 +5,43 @@ mod theme;
 use dotenv::dotenv;
 use human_panic::Metadata;
 use human_panic::setup_panic;
+use once_cell::sync::OnceCell;
 use oxy::db::client;
 use oxy::theme::StyledText;
 use std::env;
 use tracing_subscriber::{EnvFilter, fmt};
 
+static LOG_GUARD: OnceCell<tracing_appender::non_blocking::WorkerGuard> = OnceCell::new();
+
 fn init_tracing_logging(log_to_stdout: bool) {
-    let mut env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    env_filter = env_filter
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"))
         .add_directive("oxy=debug".parse().unwrap())
         .add_directive("deser_incomplete::options_impl=warn".parse().unwrap())
         .add_directive("tower_http=debug".parse().unwrap());
     let is_debug = cfg!(debug_assertions);
-    if log_to_stdout {
-        fmt()
-            .with_env_filter(env_filter)
-            .with_target(true)
-            .with_level(true)
-            .init();
-    } else {
-        let log_file_path = std::path::Path::new(&client::get_state_dir()).join("oxy.log");
-        let file_appender = tracing_appender::rolling::never(
-            log_file_path.parent().unwrap(),
-            log_file_path.file_name().unwrap(),
-        );
-        let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-        let log_builder = fmt()
-            .with_env_filter(env_filter)
-            .with_target(true)
-            .with_level(true)
-            .with_writer(non_blocking);
 
-        if !is_debug {
-            log_builder.json().init();
-        } else {
-            log_builder.init();
-        }
+    let log_file_path = std::path::Path::new(&client::get_state_dir()).join("oxy.log");
+    let file_appender = tracing_appender::rolling::never(
+        log_file_path.parent().unwrap(),
+        log_file_path.file_name().unwrap(),
+    );
+    let (non_blocking, guard) = if log_to_stdout {
+        tracing_appender::non_blocking(std::io::stdout())
+    } else {
+        tracing_appender::non_blocking(file_appender)
+    };
+    LOG_GUARD.set(guard).ok();
+    let log_builder = fmt()
+        .with_env_filter(env_filter)
+        .with_target(true)
+        .with_level(true)
+        .with_writer(non_blocking);
+
+    if !is_debug {
+        log_builder.json().init();
+    } else {
+        log_builder.init();
     }
 }
 
