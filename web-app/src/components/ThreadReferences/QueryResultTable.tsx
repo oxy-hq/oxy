9d1694535e74cd94cc77989892f5acab02a74327
Hải Trần <h@hai.fyi>
2025-05-20 15:11:43 +0700

    feat: support download query result csv (#810)
    GitOrigin-RevId: 90d91dd68df04f15c0e368deb6b1413837e69be9

--- web-app/src/components/ThreadReferences/QueryResultTable.tsx
+++ web-app/src/components/ThreadReferences/QueryResultTable.tsx
@@ -7,6 +7,9 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/shadcn/table";
+import { Button } from "../ui/shadcn/button";
+import Papa from "papaparse";
+import { Download } from "lucide-react";
 
 type ResultTableProps = {
   result: string[][];
@@ -14,8 +17,25 @@ type ResultTableProps = {
 };
 
 export const QueryResultTable = ({ result, isTruncated }: ResultTableProps) => {
+  const handleDownloadCsv = () => {
+    const csvContent = Papa.unparse(result, {
+      delimiter: ",",
+      header: true,
+      skipEmptyLines: true,
+    });
+    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "query_result.csv";
+    a.click();
+    URL.revokeObjectURL(url);
+  };
   return (
     <div className="max-h-80 overflow-auto customScrollbar">
+      <Button title="Download CSV" onClick={handleDownloadCsv}>
+        <Download />
+      </Button>
       <Table className="w-[reset]">
         <TableHeader>
           <TableRow>
