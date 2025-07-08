5ed4387ffcfa8dc693b948a8c13bfb3214d5e2d6
Hải Trần <h@hai.fyi>
2025-07-08 10:05:17 +0700

    fix: add back altifacts (#948)
    chore: add back altifacts
    GitOrigin-RevId: 4a29c26acae34cc4883e2d4fca9ae26d64fa25b6

--- crates/migration/src/lib.rs
+++ crates/migration/src/lib.rs
@@ -12,6 +12,7 @@ mod m20250523_123859_add_users_table;
 mod m20250523_123900_add_user_id_to_threads;
 mod m20250527_005652_create_table_messages;
 mod m20250609_000001_create_api_keys_table;
+mod m20250609_015141_Add_artifacts;
 mod m20250611_015638_add_tokens_to_messages;
 mod m20250613_090405_add_auth_fields_to_users;
 mod m20250618_102934_create_github_config_table;
@@ -38,6 +39,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20250523_123900_add_user_id_to_threads::Migration),
             Box::new(m20250527_005652_create_table_messages::Migration),
             Box::new(m20250609_000001_create_api_keys_table::Migration),
+            Box::new(m20250609_015141_Add_artifacts::Migration),
             Box::new(m20250611_015638_add_tokens_to_messages::Migration),
             Box::new(m20250613_090405_add_auth_fields_to_users::Migration),
             Box::new(m20250618_102934_create_github_config_table::Migration),
